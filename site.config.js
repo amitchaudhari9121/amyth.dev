2ffbfb190c5ce0c85f61979e7a4f35e3d1837e61
Amit Chaudhari <amitchaudhari9121@gmail.com>
2021-04-19 14:15:45 +0530

    Secret message Hyper encrypted, Mega Hashed and cypher blocked. You won't get to say, "I'm in."

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'e5fc573ae39b43b793f0079ea7ce08c1',
+  rootNotionPageId: "e5fc573ae39b43b793f0079ea7ce08c1",
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Amit Chaudhari',
-  domain: 'amyth.dev',
-  author: 'Amit Chaudhari',
+  name: "Amit Chaudhari",
+  domain: "amyth.dev",
+  author: "Amit Chaudhari",
 
   // open graph metadata (optional)
-  description: 'Develope\'s Log, The D-Log. I am trying to document my problems and how i tackle them, most are stupid, some are interestingly stupid.',
-  socialImageTitle: 'Amit Chaudhari',
-  socialImageSubtitle: 'A developer in making? made? no?👋',
+  description:
+    "Develope's Log, The D-Log. I am trying to document my problems and how i tackle them, most are stupid, some are interestingly stupid.",
+  socialImageTitle: "Amit Chaudhari",
+  socialImageSubtitle: "A developer in making? made? no?👋",
 
   // social usernames (optional)
-  twitter: 'amyth_dev',
-  github: 'amitchaudhari9121',
-  linkedin: 'amyth-dev',
+  twitter: "amyth_dev",
+  github: "amitchaudhari9121",
+  linkedin: "amyth-dev",
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -51,5 +52,5 @@ module.exports = {
   //  add /schedule here
   //  add /about here
   // }
-  pageUrlOverrides: null
-}
+  pageUrlOverrides: null,
+};
